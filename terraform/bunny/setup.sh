8224ec6c8bdaae9a27c478166f971df84b77ad61
Soli0222 <github@str08.net>
2025-09-21 23:09:30 +0900

    chore(bunny): すべてのファイルを削除
    - .gitignore、.terraform.lock.hcl、README.md、main.tf、provider.tf、pullzones.yaml、setup.sh、variables.tf の全ファイルを削除した。

--- terraform/bunny/setup.sh
+++
@@ -1,6 +0,0 @@
-# This script sets up the environment for the Terraform configuration
-
-export TF_VAR_bunny_api_key=$(op item get 'terraform kkg-pve' --fields label=TF_VAR_bunny_api_key --format json | jq -r .value)
-
-export AWS_ACCESS_KEY_ID=$(op item get 'terraform kkg-pve' --fields label=AWS_ACCESS_KEY_ID --format json | jq -r .value)
-export AWS_SECRET_ACCESS_KEY=$(op item get 'terraform kkg-pve' --fields label=AWS_SECRET_ACCESS_KEY --format json | jq -r .value)
